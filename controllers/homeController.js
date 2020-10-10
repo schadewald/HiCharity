f84fa4d1a9f51d039a529313cdcae73273463866
Chad Schadewald <ces83@students.uwf.edu>
2020-10-10 16:32:41 -0500

    Developing User Model.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -11,45 +11,47 @@ db.once("open", () =>
 const User = require("../models/user");
 //const Donation = require("../models/userDonation");
 
-exports.sendHomePage = (req, res) => 
+module.exports = 
 {
+    sendHomePage: (req, res) => 
+    {
     res.send("Home Page");
-};
-exports.displayRequest = (req, res) => 
-{
+    },
+    displayRequest: (req, res) => 
+    {  
     console.log(req.body);
     console.log(req.query);
     res.send("POST successful");
-};
-exports.sendLogin = (req, res) => 
-{
+    },
+    sendLogin: (req, res) => 
+    {
     //let testParam = "TEST_PARAM";
     res.render("loginPage", {name: ""}); // empty string passed to name param in layout.ejs, preventing a header/footer from loading
-};
-exports.sendUserAccount = (req, res) => 
-{
+    },
+    sendUserAccount: (req, res) => 
+    {
     res.send("User Account Page");
-};
-exports.sendDonation = (req, res) => 
-{
+    },
+    sendDonation: (req, res) => 
+    {
     res.send("Donation Page");
-};
-exports.repondWithHomePage = (req, res) => 
-{
+    },
+    repondWithHomePage: (req, res) => 
+    {
     let paramsName = req.params.myName;
     res.render("home_page", {name: paramsName});
-};
-exports.respondWithName = (req, res) => 
-{
+    },
+    respondWithName: (req, res) => 
+    {
     let paramsName = req.params.myName;
     res.render("home_page", {name: paramsName});
-};
-exports.respondWithBadRequest = (req, res) => 
-{
+    },
+    respondWithBadRequest: (req, res) => 
+    {
     res.send("Error");
-};
-exports.authenticateLoginInfo = (req, res) =>
-{
+    },
+    authenticateLoginInfo: (req, res) =>
+    {
     //console.log(req.body);
     //console.log(req.query);
 
@@ -84,4 +86,5 @@ exports.authenticateLoginInfo = (req, res) =>
     //Testing DB Operations
 
     res.send(`Login attempt: <br> Username: ${req.body.username} <br> Password: ${req.body.password}`);
+    }
 };
