89a46ceef7d04a99c029cdc02e445a1dfe785b38
Chad Schadewald <ces83@students.uwf.edu>
2020-12-09 19:00:38 -0600

    Trying to get containers to work.

--- front-end/routes/userRoutes.js
+++ front-end/routes/userRoutes.js
@@ -1,33 +1,35 @@
 const router = require("express").Router(),
     userController = require("../controllers/userController");
-const axios = require("axios");
+// const axios = require("axios");
 
-router.use("/", (req, res) => 
-{
-    console.log(`Front-End request to API made to: ${req.url}`);
-    axios
-    .get(process.env.API_ENDPOINT + `users${req.url}`)
-    .then((response) => console.log("API Called From Front-End User Route."))
-    .catch((err) => console.log(err));
-});
+// router.use("/", (req, res) => 
+// {
+//     console.log(`Front-End request to APImade to: ${req.url}`);
+//     axios
+//     .get(process.env.API_ENDPOINT + `users${req.url}`)
+//     .then((response) => console.log("API Called From Front-End User Route."))
+//     .catch((err) => console.log(err));
+// });
+
 //localhost:8080/userList
+router.get("/userList", userController.index, userController.indexView);
     
-// router.get("/newuser", userController.new);
+router.get("/newuser", userController.new);
     
-// router.post("/newuser", userController.validate, userController.create, userController.redirectView);
+router.post("/newuser", userController.validate, userController.create, userController.redirectView);
     
-// router.get("/login", userController.login);
+router.get("/login", userController.login);
     
-// router.post("/login", userController.authenticate);
+router.post("/login", userController.authenticate);
     
-// router.get("/logout", userController.logout, userController.redirectView);
+router.get("/logout", userController.logout, userController.redirectView);
     
-// router.get("/:id", userController.show, userController.showView);
+router.get("/:id", userController.show, userController.showView);
     
-// router.get("/:id/edit", userController.edit);
+router.get("/:id/edit", userController.edit);
     
-// router.put("/:id/update", userController.update, userController.redirectView);
+router.put("/:id/update", userController.update, userController.redirectView);
     
-// router.delete("/:id/delete", userController.delete, userController.redirectView);
+router.delete("/:id/delete", userController.delete, userController.redirectView);
 
 module.exports = router;
